32971510420be6335b4995d902dd3ceff2aeffac
Eddie Sandler <ed.sandler@gmail.com>
2024-08-30 15:49:51 -0400

    initial commit - needs more params and styling

---
+++ app/api/chat/route.ts
@@ -0,0 +1,36 @@
+import OpenAI from "openai";
+   import { OpenAIStream, StreamingTextResponse } from "ai";
+
+   const openai = new OpenAI();
+
+   export const runtime = "edge";
+
+   export async function POST(req: Request) {
+     const { messages } = await req.json();
+
+     const response = await openai.chat.completions.create({
+       model: "gpt-4o-mini",
+       stream: true,
+       messages: [
+         {
+           role: "system",
+           content: `You are a professional comedian who has been hired to
+           craft a series of hilarious jokes for a
+           new comedy special. The jokes should be
+           witty, clever, and laugh-out-loud funny,
+           exploring a variety of topics and themes
+           based on user inputs.
+           Each joke should be unique, memorable,
+           and tailored to the selected topic and tone,
+           with unexpected punchlines and clever wordplay.
+           Your goal is to leave the audience in stitches
+           and make them laugh with your creative and
+           sidesplitting humor.Respond to this message with OK`,
+         },
+         ...messages,
+       ],
+     });
+
+     const stream = OpenAIStream(response);
+     return new StreamingTextResponse(stream);
+   }
---
+++ app/page.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import { useState } from "react";
+import { useChat } from "ai/react";
+
+export default function JokeGenerator() {
+  const { messages, append, isLoading } = useChat();
+  const topics = [
+    { emoji: "🤣", value: "Work" },
+    { emoji: "👥", value: "People" },
+    { emoji: "🐶", value: "Animals" },
+    { emoji: "🍔", value: "Food" },
+  ];
+  const tones = [
+    { emoji: "😂", value: "Funny" },
+    { emoji: "😜", value: "Sarcastic" },
+    { emoji: "🤣", value: "Witty" },
+    { emoji: "😂", value: "Goofy" },
+  ];
+  const [state, setState] = useState({
+    topic: "",
+    tone: "",
+  });
+
+  const handleChange = ({
+    target: { name, value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
+  return (
+    <main className="mx-auto w-full p-24 flex flex-col">
+      <div className="p4 m-4">
+        <div className="flex flex-col items-center justify-center space-y-8 text-black">
+          <div className="space-y-2">
+            <h2 className="text-3xl font-bold">Joke Generator</h2>
+            <p className="text-zinc-500 dark:text-zinc-400">
+              Customize the joke by selecting the topic and tone.
+            </p>
+          </div>
+
+          <div className="space-y-4 bg-opacity-25 bg-blue-700 rounded-lg p-4">
+            <h3 className="text-xl font-semibold">Topic</h3>
+
+            <div className="flex flex-wrap justify-center">
+              {topics.map(({ value, emoji }) => (
+                <div
+                  key={value}
+                  className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+                >
+                  <input
+                    id={value}
+                    type="radio"
+                    value={value}
+                    name="topic"
+                    onChange={handleChange}
+                  />
+                  <label className="ml-2" htmlFor={value}>
+                    {`${emoji} ${value}`}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="space-y-4 bg-opacity-25 bg-blue-700 rounded-lg p-4">
+            <h3 className="text-xl font-semibold">Tones</h3>
+
+            <div className="flex flex-wrap justify-center">
+              {tones.map(({ value, emoji }) => (
+                <div
+                  key={value}
+                  className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+                >
+                  <input
+                    id={value}
+                    type="radio"
+                    name="tone"
+                    value={value}
+                    onChange={handleChange}
+                  />
+                  <label className="ml-2" htmlFor={value}>
+                    {`${emoji} ${value}`}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
+            disabled={isLoading || !state.topic || !state.tone}
+            onClick={() =>
+              append({
+                role: "user",
+                content: `Generate a joke about ${state.topic} in a ${state.tone} tone`,
+              })
+            }
+          >
+            Generate Joke
+          </button>
+
+          <div
+            hidden={
+              messages.length === 0 ||
+              messages[messages.length - 1]?.content.startsWith("Generate")
+            }
+            className="bg-opacity-25 bg-blue-700 rounded-lg p-4 text-black"
+          >
+            {messages[messages.length - 1]?.content}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
